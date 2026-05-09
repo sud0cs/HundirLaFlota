14ec26b64e9f059235b6066e234e7952d1ee1343
sud0cs <asintessintes@gmail.com>
2026-05-09 09:21:57 +0200

    detectar i pintar casella

---
+++ app/src/main/java/com/example/hundirlaflota/Casella.java
@@ -0,0 +1,10 @@
+package com.example.hundirlaflota;
+
+public class Casella {
+    int x;
+    int y;
+    public Casella(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
--- app/src/main/java/com/example/hundirlaflota/MainActivity.java
+++ app/src/main/java/com/example/hundirlaflota/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -111,19 +113,13 @@ public class MainActivity extends AppCompatActivity {
             tip.setEnabled(true);
         }
     }
-
-    public void pintar(SurfaceView tauler, int files, int columnes){
-        if(tauler.getHolder().getSurface().isValid()){
+    public void pintar(SurfaceView tauler, int files, int columnes, Canvas canvas){
             int alt = tauler.getHeight();
             int ampla = tauler.getWidth();
-
-            Canvas canvas = tauler.getHolder().lockCanvas();
             canvas.drawColor(Color.parseColor("#ADD8E6"));
-
             Paint p = new Paint();
             p.setColor(Color.WHITE);
             p.setStrokeWidth(3);
-
             float midaX = ampla/(float) columnes;
             float midaY = alt/(float) files;
 
@@ -131,13 +127,62 @@ public class MainActivity extends AppCompatActivity {
                 float x = i*midaX;
                 canvas.drawLine(x,0,x,alt,p);
             }
-
             for(int j = 0; j<=files; j++){
                 float y = j*midaY;
                 canvas.drawLine(0,y,ampla,y,p);
             }
-
+    }
+    public void pintar(SurfaceView tauler, int files, int columnes){
+        if(tauler.getHolder().getSurface().isValid()){
+            Canvas canvas = tauler.getHolder().lockCanvas();
+            pintar(tauler, files, columnes, canvas);
             tauler.getHolder().unlockCanvasAndPost(canvas);
         }
+    }
+
+    private void drawSingleGrid(Casella c, SurfaceView tauler){
+        if(tauler.getHolder().getSurface().isValid()){
+            Canvas canvas = tauler.getHolder().lockCanvas();
+            /*
+            Hardcoded amount of rows and columns.
+            Maybe change that?
+             */
+            pintar(tauler, 10, 10, canvas);
+            float x = tauler.getWidth()/(float)10;
+            float y = tauler.getHeight()/(float)10;
+            Paint p = new Paint();
+            p.setStyle(Paint.Style.STROKE);
+            p.setStrokeWidth(5);
+            p.setColor(Color.RED);
+            canvas.drawRoundRect(c.x*x,c.y*y,c.x*x + x,c.y*y + y,25,25,p);
+            tauler.getHolder().unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public void drawGrids(Casella c){
+        drawSingleGrid(c, taulerIntents);
+    }
+
+    private Casella getCasella(float x, float y){
+        int posX = (int)Math.floor(((x - taulerIntents.getX())/taulerIntents.getWidth())*10);
+        int posY = (int)Math.floor(((y - taulerIntents.getY())/taulerIntents.getHeight())*10);
+        missatges.append(String.format("\nCASELLA: (%s,%s)", posX, posY));
+        DarreraJugada2.setText(String.format("Seleccionada la casella (%s, %s)", posX, posY));
+        return new Casella(posX, posY);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(
+                event.getX()>=taulerIntents.getX() &&
+                event.getX()<=taulerIntents.getX()+taulerIntents.getWidth() &&
+                event.getY()>=taulerIntents.getY() &&
+                event.getY()<=taulerIntents.getY()+taulerIntents.getHeight()
+        ){
+            if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_UP) {
+                drawGrids(getCasella(event.getX(), event.getY()));
+            }
+        }
+        return true;
     }
 }
